b2ddbf9e4f3f256a6b16537ba4ffc0602648cc15
ClaudiaN1 <nicoricicllaudia@yahoo.ro>
2021-01-05 17:29:55 +0000

    variables and types

--- aHelloWorld/strings.py
+++ aHelloWorld/strings.py
@@ -5,9 +5,19 @@ print('We can even include "quotes" in strings')
 print("hello" + " world")
 
 greeting = "Hello"
-# name = "Bruce"
-name = input("Please enter your name ")
+name = "Bruce"
+# name = input("Please enter your name ")
 print(greeting + name)
 
 # if we want a space, we can add that too
 print(greeting + ' ' + name)
+
+age = 24
+print(age)
+
+print(type(greeting))
+print(type(age))
+
+age = "2 years"
+print(age)
+print(type(age))
