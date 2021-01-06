f1b37078f4a9f877dbbd2fc662b6274317ee2774
ClaudiaN1 <nicoricicllaudia@yahoo.ro>
2021-01-06 18:59:40 +0000

    slicing

---
+++ aHelloWorld/slicing.py
@@ -0,0 +1,21 @@
+#                   1
+#         012345678901234
+parrot = "Norwegian Blue"
+
+print(parrot[0:6])  # Norweg
+print(parrot[3:5])  # we
+print(parrot[0:9])  # Norwegian
+print(parrot[:9])   # Norwegian
+
+print(parrot[-4:])  # Blue
+# or
+print(parrot[10:14])
+# or
+print(parrot[10:])
+
+print(parrot[:6])   # Norweg
+print(parrot[6:])   # ian Blue
+
+print(parrot[:6] + parrot[6:])  # Norwegian Blue
+
+print(parrot[:])    # Norwegian Blue
