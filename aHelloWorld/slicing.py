d48c178682889efb21bddce869e6dba90c23f702
ClaudiaN1 <nicoricicllaudia@yahoo.ro>
2021-01-14 21:28:49 +0000

    step in a slice

--- aHelloWorld/slicing.py
+++ aHelloWorld/slicing.py
@@ -27,3 +27,14 @@ print(parrot[-8:])  # ian Blue
 print(parrot[:6] + parrot[6:])  # Norwegian Blue
 
 print(parrot[:])  # Norwegian Blue
+
+print(parrot[0:6:2])   # Nre -> we step through the sequence in steps of 2
+print(parrot[0:6:3])   # Nw -> steps of 3
+print(parrot[1::4])    # og e
+
+number = "9,223;372:036 854,775;807"
+separators = number[1::4]
+print(separators)   # ,;: ,;
+
+values = "".join(char if char not in separators else " " for char in number).split()
+print([int(val)for val in values])  # [9, 223, 372, 36, 854, 775, 807]
