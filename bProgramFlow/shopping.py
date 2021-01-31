546edd8805c7ff5a6b56e9190360f4e7665d9455
ClaudiaN1 <nicoricicllaudia@yahoo.ro>
2021-01-31 00:21:43 +0000

    break

--- bProgramFlow/shopping.py
+++ bProgramFlow/shopping.py
@@ -9,3 +9,22 @@ for item in shopping_list:
         continue
 
     print("Buy " + item)
+print("-------------------")
+
+# break
+for item in shopping_list:
+    if item == "spam":
+        break
+
+    print("Buy " + item)
+print("-------------------")
+
+# searching
+item_to_find = "spam"
+found_at = None
+
+for index in range(len(shopping_list)):
+    if shopping_list[index] == item_to_find:
+        found_at = index
+        break
+print("Item found at position {}".format(found_at))
