e42409fec6b238037a0a0407fc39cb61804dca51
ClaudiaN1 <nicoricicllaudia@yahoo.ro>
2021-02-01 21:39:12 +0000

    changed the program with while loop

--- bProgramFlow/guessinggame.py
+++ bProgramFlow/guessinggame.py
@@ -3,23 +3,38 @@ import random
 highest = 10
 answer = random.randint(1, highest)
 print(answer)  # TODO: Remove after testing
-
+guess = 0   # initialise to any number that doesn't equal the answer
 print("Please guess number between 1 and {}: ".format(highest))
-guess = int(input())
 
-if guess != answer:
-    if guess < answer:
-        print("Please guess higher")
-    else:
-        print("Please guess lower")
+while guess != answer:
     guess = int(input())
+
+    if guess == 0:
+        break
     if guess == answer:
         print("We ll done, you guessed it")
+        break
     else:
-        print("Sorry, you have not guessed correctly")
-else:
-    print("You got it first time")
+        if guess < answer:
+            print("Please guess higher")
+        else:   # guess must be greater than answer
+            print("Please guess lower")
 
+# ------------------------------------------------------------
+# if guess != answer:
+#     if guess < answer:
+#         print("Please guess higher")
+#     else:
+#         print("Please guess lower")
+#     guess = int(input())
+#     if guess == answer:
+#         print("We ll done, you guessed it")
+#     else:
+#         print("Sorry, you have not guessed correctly")
+# else:
+#     print("You got it first time")
+
+# ------------------------------------------------------------
 # if guess < answer:
 #     print("Please guess higher")
 #     guess = int(input())
